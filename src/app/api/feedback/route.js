6c1c237840d7a053b79ef619507d017baaa04c87
j4ckgrey <j4ckgrey@gmail.com>
2025-06-04 21:46:08 +0200

    fixed route

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -39,6 +39,7 @@ export async function POST(req) {
       );
     }
 
+    const protocol = "https";
     const host = process.env.NEXT_PUBLIC_SITE_URL;
     const confirmUrl = `${protocol}://${host}/confirm?token=${token}`;
 
